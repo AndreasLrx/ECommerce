062e903e3e5f3cfd9284c5efd5f928dac723c6f2
Andreas Leroux <andreas.leroux@epitech.eu>
2023-04-18 16:21:36 +0200

    Document OrderController endpoints
    Linked: #33

--- app/src/Controller/API/OrderController.php
+++ app/src/Controller/API/OrderController.php
@@ -10,18 +10,48 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Order;
 use Symfony\Component\HttpFoundation\Response;
 use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 #[OA\Tag('Orders', "Endpoints related to the user orders")]
 #[Route('/orders')]
 class OrderController extends BaseController
 {
     #[Route('/', methods: ['GET'])]
+    #[OA\Get(description: 'Get the user orders')]
+    #[OA\Response(
+        response: 200,
+        description: 'Success',
+        content: new OA\JsonContent(type: 'array', items: new OA\Items(ref: new Model(type: Order::class, groups: ["default"])))
+    )]
     public function get_user_orders(UserRepository $userRepository): JsonResponse
     {
         return $this->json($this->get_user_entity($userRepository)->getOrders()->toArray());
     }
 
     #[Route('/{order<\d+>}', methods: ['GET'])]
+    #[OA\Get(description: 'Fetch a specific user order')]
+    #[OA\Parameter(
+        name: "order",
+        in: 'path',
+        required: true,
+        description: 'ID of the order to fetch',
+        schema: new OA\Schema(type: 'integer')
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Success',
+        content: new Model(type: Order::class, groups: ["default"])
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Order doesn't belong to the authentified user",
+        content: new OA\JsonContent(ref: '#components/schemas/GeneralError')
+    )]
+    #[OA\Response(
+        response: 404,
+        description: "Order doesn't exist",
+        content: new OA\JsonContent(ref: '#components/schemas/GeneralError')
+    )]
     public function get_user_order(UserRepository $userRepository, Order $order): JsonResponse
     {
         if ($this->get_user_entity($userRepository) != $order->getUser())
