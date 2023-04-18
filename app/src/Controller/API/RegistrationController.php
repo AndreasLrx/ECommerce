d53e36d3a4c1ec856f35f822d51d58bf803b539c
Andreas Leroux <andreas.leroux@epitech.eu>
2023-04-18 17:07:25 +0200

    Document RegistrationController endpoints (and api/login)
    Linked: #33

--- app/src/Controller/API/RegistrationController.php
+++ app/src/Controller/API/RegistrationController.php
@@ -15,12 +15,36 @@ use App\Repository\UserRepository;
 use App\Request\RegistrationRequest;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use OpenApi\Attributes as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
+
 
 #[OA\Tag('Authentication', "Endpoints related to user authentication")]
 class RegistrationController extends BaseController
 {
     #[Route('/register', methods: ['POST'])]
     #[Security(name: null)]
+    #[OA\Post(description: 'Register a new user')]
+    #[OA\RequestBody(description: "User informations", required: true, content: new Model(type: User::class, groups: ["default", "update"]))]
+    #[OA\Response(
+        response: 201,
+        description: 'Success',
+        content: new OA\JsonContent(type: "object", properties: [
+            new OA\Property(
+                'message',
+                type: 'string'
+            ),
+            new OA\Property(
+                'token',
+                type: 'string',
+                description: "Bearer Token"
+            )
+        ])
+    )]
+    #[OA\Response(
+        response: 422,
+        description: "User informations are invalid",
+        content: new OA\JsonContent(ref: '#components/schemas/GeneralError')
+    )]
     public function register(UserRepository $repository, CartRepository $cartRepository, UserPasswordHasherInterface $passwordHasher, RegistrationRequest $request, JWTTokenManagerInterface $JWTManager): JsonResponse
     {
         // Login must be unique
